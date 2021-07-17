a6902131b57f581017ac7913013c0e7dfc5a952c
Niharika-Sunnam <85931156+Niharika-Sunnam@users.noreply.github.com>
2021-07-17 09:26:02 +0530

    Add files via upload

---
+++ arthematic oper convert feet to inches.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int feet,inches;
+	printf("Enter the value of feet: ");
+	scanf("%d",&feet);
+	inches = feet*12;
+	printf("\nTotal inches will be: %d\n",inches);
+	return 0;
+}

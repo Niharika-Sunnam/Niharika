459b82c9d6a414f332d5f68172b026599293685b
Niharika-Sunnam <85931156+Niharika-Sunnam@users.noreply.github.com>
2021-07-18 22:32:55 +0530

    Add files via upload

---
+++ while loop natural numbers 1 to n.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int num, count = 1;
+	printf("Enter a positive number\n");
+	scanf("%d", &num);
+	printf("\nNatural numbers from %d to %d:\n", count, &num);
+	while(count <= num)
+	{
+		printf("%d ", count);
+		count++;
+	}
+	
+	printf("\n");
+	
+	return 0;
+}

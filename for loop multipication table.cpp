f4e5c62d60bc7e972513014a95a36de591a61b86
Niharika-Sunnam <85931156+Niharika-Sunnam@users.noreply.github.com>
2021-07-18 22:22:36 +0530

    Add files via upload

---
+++ for loop multipication table.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int j,n;
+	printf("Input the number (Table to be calculated) : ");
+	scanf("%d",&n);
+	printf("\n", j<=10);
+	for(j=1;j<=10;++j)
+	{
+		printf("%d * %d = %d \n", n, j, n * j);
+		
+		return 0;
+	}
+}

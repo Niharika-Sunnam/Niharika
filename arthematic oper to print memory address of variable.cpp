b7e17e221bc11b7117b875635c3d9ffbdea846cc
Niharika-Sunnam <85931156+Niharika-Sunnam@users.noreply.github.com>
2021-07-19 21:59:21 +0530

    Add files via upload

---
+++ arthematic oper to print memory address of variable.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void)
+{
+	int a;
+	float b;
+    char c;
+    printf("Address of a: %d\n", &a);
+    printf("Address of b: %d\n", &b);
+    printf("Address of c: %d\n", &c);
+     return 0;
+}
